04a886390c473b4ebf4829fd54a10f3bd79f4b3c
Nikita Maximov <nikita_maximov@relex.ru>
2019-03-17 19:12:17 +0300

    first

---
+++ StringSearch/Form1.Designer.cs
@@ -0,0 +1,156 @@
+﻿namespace StringSearch
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.tableLayoutPanel2 = new System.Windows.Forms.TableLayoutPanel();
+            this.tableLayoutPanel3 = new System.Windows.Forms.TableLayoutPanel();
+            this.search = new System.Windows.Forms.Button();
+            this.openFile = new System.Windows.Forms.Button();
+            this.patternField = new System.Windows.Forms.RichTextBox();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.tableLayoutPanel1.SuspendLayout();
+            this.tableLayoutPanel2.SuspendLayout();
+            this.tableLayoutPanel3.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.ColumnCount = 1;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.Controls.Add(this.tableLayoutPanel2, 0, 1);
+            this.tableLayoutPanel1.Controls.Add(this.panel1, 0, 0);
+            this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 2;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 79.55556F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 20.44444F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(800, 450);
+            this.tableLayoutPanel1.TabIndex = 0;
+            // 
+            // tableLayoutPanel2
+            // 
+            this.tableLayoutPanel2.ColumnCount = 2;
+            this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel2.Controls.Add(this.tableLayoutPanel3, 1, 0);
+            this.tableLayoutPanel2.Controls.Add(this.patternField, 0, 0);
+            this.tableLayoutPanel2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel2.Location = new System.Drawing.Point(3, 361);
+            this.tableLayoutPanel2.Name = "tableLayoutPanel2";
+            this.tableLayoutPanel2.RowCount = 1;
+            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel2.Size = new System.Drawing.Size(794, 86);
+            this.tableLayoutPanel2.TabIndex = 0;
+            // 
+            // tableLayoutPanel3
+            // 
+            this.tableLayoutPanel3.ColumnCount = 2;
+            this.tableLayoutPanel3.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel3.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel3.Controls.Add(this.search, 0, 0);
+            this.tableLayoutPanel3.Controls.Add(this.openFile, 1, 0);
+            this.tableLayoutPanel3.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel3.Location = new System.Drawing.Point(400, 3);
+            this.tableLayoutPanel3.Name = "tableLayoutPanel3";
+            this.tableLayoutPanel3.RowCount = 1;
+            this.tableLayoutPanel3.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel3.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel3.Size = new System.Drawing.Size(391, 80);
+            this.tableLayoutPanel3.TabIndex = 0;
+            // 
+            // search
+            // 
+            this.search.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.search.Location = new System.Drawing.Point(3, 3);
+            this.search.Name = "search";
+            this.search.Size = new System.Drawing.Size(189, 74);
+            this.search.TabIndex = 0;
+            this.search.Text = "Поиск";
+            this.search.UseVisualStyleBackColor = true;
+            this.search.Click += new System.EventHandler(this.search_Click);
+            // 
+            // openFile
+            // 
+            this.openFile.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.openFile.Location = new System.Drawing.Point(198, 3);
+            this.openFile.Name = "openFile";
+            this.openFile.Size = new System.Drawing.Size(190, 74);
+            this.openFile.TabIndex = 1;
+            this.openFile.Text = "Открыть";
+            this.openFile.UseVisualStyleBackColor = true;
+            this.openFile.Click += new System.EventHandler(this.openFile_Click);
+            // 
+            // patternField
+            // 
+            this.patternField.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.patternField.Location = new System.Drawing.Point(3, 3);
+            this.patternField.Name = "patternField";
+            this.patternField.Size = new System.Drawing.Size(391, 80);
+            this.patternField.TabIndex = 1;
+            this.patternField.Text = "";
+            // 
+            // panel1
+            // 
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel1.Location = new System.Drawing.Point(3, 3);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(794, 352);
+            this.panel1.TabIndex = 1;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.tableLayoutPanel2.ResumeLayout(false);
+            this.tableLayoutPanel3.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel2;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel3;
+        private System.Windows.Forms.Button search;
+        private System.Windows.Forms.RichTextBox patternField;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button openFile;
+    }
+}
+
---
+++ StringSearch/Form1.cs
@@ -0,0 +1,254 @@
+﻿using SearchResultsSpace;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace StringSearch
+{
+
+
+    public partial class Form1 : Form
+    {
+        public string fileName = "";
+        public List<SearchResults> searchResults;
+        public string text;
+
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void openFile_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog theDialog = new OpenFileDialog();
+            theDialog.Title = "Open Text File";
+            theDialog.Filter = "TXT files|*.txt";
+            theDialog.InitialDirectory = @"C:\Users\Kathrine\Desktop";
+            if (theDialog.ShowDialog() == DialogResult.OK)
+            {
+                fileName = theDialog.FileName.ToString();
+                text = File.ReadAllText(fileName, Encoding.GetEncoding(1251));
+            }
+        }
+
+        private void search_Click(object sender, EventArgs e)
+        {
+            string pattern = patternField.Text;
+            searchResults = new List<SearchResults>();
+            searchResults.Add(nativSearch(this.text, pattern));
+            searchResults.Add(knpSearch(this.text, pattern));
+            searchResults.Add(bmSearch(this.text, pattern));
+            searchResults.Add(shiftAndSearch(this.text, pattern));
+        }
+
+        private SearchResults nativSearch(string text, string pattern)
+        {
+            int count = 0;
+            int time = 0;
+            var before = DateTime.Now;
+
+            for (var i = 0; i < text.Length - pattern.Length; i++)
+            {
+                if (text.Substring(i, pattern.Length) == pattern)
+                {
+                    count++;
+                }
+            }
+       
+
+            var after = DateTime.Now;
+            time = (int) (dateToMs(after) - dateToMs(before));
+            return new SearchResults
+            {
+                Name = "Нативный алгоритм",
+                Time = time / 1000,
+                Count = count
+            };
+        }
+
+        private SearchResults knpSearch(string text, string pattern)
+        {
+            int count = 0;
+            int time = 0;
+            var before = DateTime.Now;
+
+
+            var pf = prefixFunction(pattern + '@' + text);
+            for (var i = 0; i < text.Length - 1; i++)
+            {
+                if (pf[pattern.Length + i + 1] == pattern.Length)
+                {
+                    count++;
+                }
+            }
+
+            var after = DateTime.Now;
+            time = (int)(dateToMs(after) - dateToMs(before));
+            return new SearchResults
+            {
+                Name = "Алгоритм Кнута-Морриса-Пратта",
+                Time = time / 1000,
+                Count = count
+            };
+        }
+
+        private int[] prefixFunction(String str)
+        {
+            int[] prefixFunc = new int[str.Length];
+            prefixFunc[0] = 0;
+            for (var i = 1; i < str.Length - 1; i++)
+            {
+                var k = prefixFunc[i - 1];
+                while (k > 0 && str[i] != str[k])
+                {
+                    k = prefixFunc[k - 1];
+                }
+                if (str[i] == str[k])
+                {
+                    k++;
+                }
+                prefixFunc[i] = k;
+            }
+            return prefixFunc;
+        }
+
+        private SearchResults bmSearch(string text, string pattern)
+        {
+            int count = 0;
+            int time = 0;
+            var before = DateTime.Now;
+
+            var textLen = text.Length;
+            var patternLen = pattern.Length;
+            if (textLen < patternLen)
+            {
+                return null;
+            }
+            var map = new Dictionary<char, int>();
+            for (var t = 0; t < patternLen - 1; t++)
+            {
+                map.Add(pattern[t], patternLen - t - 1);
+            }
+            var i = patternLen - 1;
+            var j = i;
+            var k = i;
+            while (i <= textLen - 1)
+            {
+                j = patternLen - 1;
+                k = i;
+                while (j >= 0 && text[k] == pattern[j])
+                {
+                    j--;
+                    k--;
+                }
+                if (j == -1)
+                {
+                    count++;
+                }
+                i += map.ContainsKey(text[i]) ? map[text[i]] : patternLen;
+            }
+
+            var after = DateTime.Now;
+            time = (int)(dateToMs(after) - dateToMs(before));
+            return new SearchResults
+            {
+                Name = "Алгоритм Кнута-Морриса-Пратта",
+                Time = time / 1000,
+                Count = count
+            };
+        }
+
+        private SearchResults shiftAndSearch(string text, string pattern)
+        {
+            int count = 0;
+            int time = 0;
+            var before = DateTime.Now;
+            using (MD5 md5Hash = MD5.Create())
+            {
+                var hashText = GetMd5Hash(md5Hash, text.Substring(0, pattern.Length));
+                var hashPattern = GetMd5Hash(md5Hash, pattern);
+                for (var i = 0; i < text.Length - pattern.Length; i++)
+                {
+                    if (VerifyMd5Hash(md5Hash, hashText, hashPattern))
+                    {
+                        count++;
+                    }
+                    hashText = GetMd5Hash(md5Hash, text.Substring(i, pattern.Length));
+                }
+            }
+           
+
+            var after = DateTime.Now;
+            time = (int)(dateToMs(after) - dateToMs(before));
+            return new SearchResults
+            {
+                Name = "Алгоритм Кнута-Морриса-Пратта",
+                Time = time / 1000,
+                Count = count
+            };
+        }
+
+
+        private long dateToMs(DateTime date)
+        {
+            return (long)(date - new DateTime(1970, 1, 1)).TotalMilliseconds;
+        }
+
+
+        static string GetMd5Hash(MD5 md5Hash, string input)
+        {
+
+            // Convert the input string to a byte array and compute the hash.
+            byte[] data = md5Hash.ComputeHash(Encoding.UTF8.GetBytes(input));
+
+            // Create a new Stringbuilder to collect the bytes
+            // and create a string.
+            StringBuilder sBuilder = new StringBuilder();
+
+            // Loop through each byte of the hashed data 
+            // and format each one as a hexadecimal string.
+            for (int i = 0; i < data.Length; i++)
+            {
+                sBuilder.Append(data[i].ToString("x2"));
+            }
+
+            // Return the hexadecimal string.
+            return sBuilder.ToString();
+        }
+
+        // Verify a hash against a string.
+        static bool VerifyMd5Hash(MD5 md5Hash, string hash1, string hash2)
+        {
+            // Create a StringComparer an compare the hashes.
+            StringComparer comparer = StringComparer.OrdinalIgnoreCase;
+
+            if (0 == comparer.Compare(hash1, hash2))
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
+
+namespace SearchResultsSpace
+{
+    public class SearchResults
+    {
+        public String Name { get; set; }
+        public int Time { get; set; }
+        public int Count { get; set; }
+    }
+}
